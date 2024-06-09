afe1c1f65d20a50aff9ec9aca3d5785d41321d2a
Dempapa <63452778+49n3r@users.noreply.github.com>
2024-06-09 18:34:17 -0400

    Update Layered_Carbon_Initiator.py

--- Layered_Carbon_Initiator.py
+++ Layered_Carbon_Initiator.py
@@ -7,7 +7,7 @@ import plotly
 import plotly.express as px
 
 ### This Application builds random initial models for molecular dynamics simulation of 
-#amorphous graphie, multi-shell fullerenes and carbon nanotubes
+#amorphous graphite, multi-shell fullerenes and carbon nanotubes
 
 st.set_page_config(
     page_title="MTG-OU",
@@ -21,18 +21,18 @@ st.sidebar.image(logo_url)
 
 st.markdown(
     """
-    This app designed by the Materials Theory Group - Ohio University (MTG-OU) builds random 
-    initial models for molecular dynamics (MD) simulation of amorphous graphite, multi-shell fullerenes
-    and carbon nanotubes.
+    This app collection, designed by the Materials Theory Group - Ohio University (MTG-OU), constructs 
+    initial models for molecular dynamics (MD) simulation of different carbon structures including
+    amorphous graphite, multi-shell fullerenes, multi-walled carbon nanotubes, and porous carbon.
 
     **👈 Select a constructor from the sidebar** to build your initial models for MD simulations!
-    ### Want to learn more aboout MTG-OU?
-    - Check out our [resarch](https://daviddrabold.com/research/)
+    ### Want to learn more about MTG-OU?
+    - Check out our [research](https://daviddrabold.com/research/)
     - Jump into our [publications](https://daviddrabold.com/publications/)
     - Enjoy some cool animations on our [YouTube](https://youtube.com/playlist?list=PLkk5oFUpZDlbSOc7lLYdTramEX_Y3jx8m&si=UIAPj_2G2cCTeaUH) channel.
-    ### Check out other MTG-OU apps
-    - This [streamlit app](https://porous-carbon-constructor.streamlit.app/) (also included here 👈) constructs atomistic porous carbon models which 
-      can be converted to continuum models for finite element simulation. See the associated publication here.
+    # ### Check out other MTG-OU apps
+    # - This [streamlit app](https://porous-carbon-constructor.streamlit.app/) (also included here 👈) constructs atomistic porous carbon models which 
+    #   can be converted to continuum models for finite element simulation. See the associated publication here.
 """
 )
 
